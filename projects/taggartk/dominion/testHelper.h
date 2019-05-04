62f2e0e2218ce9757b84e5485612d8a641d9746d
Keenan Taggart <taggartk@oregonstate.edu>
2019-05-03 22:44:44 -0700

    Added auxiliary functions to help with testing. Basically just prints in colors and handles repeated basic logic.

---
+++ projects/taggartk/dominion/testHelper.h
@@ -0,0 +1,11 @@
+#ifndef TEST_HELPER_H
+#define TEST_HELPER_H
+
+#include "dominion.h"
+
+void printCorrect();
+void printFailure();
+int getSupplyCount(struct gameState *g); 
+void myAssert();
+
+#endif
